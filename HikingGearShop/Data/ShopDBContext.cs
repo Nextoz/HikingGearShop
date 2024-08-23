18f37fad6f588293ac60a9b3f421f60fba2184fb
Evgeny Karpov <evkar91@gmail.com>
2024-08-23 22:43:46 +0200

    Add project files.

---
+++ HikingGearShop/Data/ShopDBContext.cs
@@ -0,0 +1,24 @@
+﻿using HikingGearShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HikingGearShop.Data
+{
+    public class ShopDBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+
+        public ShopDBContext(DbContextOptions<ShopDBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+             //TODO define index and maybe seed data here 
+            );
+        }
+
+    }
+}
---
+++ HikingGearShop/Models/CartItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HikingGearShop.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
